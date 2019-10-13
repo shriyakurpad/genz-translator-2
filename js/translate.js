20d978bd9c502aaaa1e384d924836ebd321a220c
srkurpad <srkurpad@cs.washington.edu>
2019-10-13 01:25:04 -0700

    matched firebase to user input! DAB

--- js/translate.js
+++ js/translate.js
@@ -10,23 +10,59 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 
-let map = firebase.database();
+let btn = document.getElementById("my-btn");
+btn.addEventListener("click", translate);
+
+let dict = firebase.database();
 
 function testDB(){
-  map.ref("mans").once('value').then(function(snapshot){
+  dict.ref("mans").once('value').then(function(snapshot){
     let str = snapshot.val();
     console.log(str)
   });
+}
+
+function translate() {
+    let sentence = document.getElementById('textinput').value;
+    // console.log(sentence)
+    // let sentenceArray = sentence.split(" ");
+    // let word = "";
+    // console.log(sentence);
+    let leadsRef = dict.ref('lingo');
+    leadsRef.on('value', function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          let key = childSnapshot.key; // "ada"
+          let lingo = snapshot.child("lingo/"+key).key;
+          let word = childSnapshot.val();
+          // console.log(lingo);
+
+           if (sentence.includes(lingo)) {
+               console.log("key:" + lingo + " val:" + word);
+           }
+
+        });
+    });
+
+
+    // for (let i in sentenceArray) {
+    //   word = sentenceArray[i];
+    //   console.log(word);
+    // }
+    // var leadsRef = dict.ref('lingo');
+    // leadsRef.on('value', function(snapshot) {
+    //     snapshot.forEach(function(childSnapshot) {
+    //       var childData = childSnapshot.val();
+    //       console.log(childData);
+    //     });
+    // });
+    // dict.once('value').then(function(snapshot){
+    //   console.log(snapshot.val());
+    // });
 
 }
+
 
 // check the input with the database
-var button = document.querySelector('input');
-button.addEventListener('click', translate(sentence));
-
-function translate(var sentence) {
-    var result = "";
-    for (var word : sentence) {
-
-    }
-}
+// document.getElementById('submit').onclick = function() {
+//     console.log("TEST");
+// }
